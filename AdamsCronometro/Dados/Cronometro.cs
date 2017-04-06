efe733ec49614c6ba01b43f9291ce90a6285884f
Lucas Adams <adams.lucass@gmail.com>
2017-04-06 20:40:31 -0300

    cronometro quase pronto, falta ligar com BD

---
+++ AdamsCronometro/Dados/Cronometro.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.ComponentModel;
+
+namespace AdamsCronometro.Dados
+{
+    [Table]
+    class Cronometro : INotifyPropertyChanged
+    {
+        #region id
+        private int id;
+
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (value != id)
+                {
+                    id = value;
+                    NotifyPropertyChanged("ID");
+                }
+            }
+        }
+        #endregion
+
+        #region tempo
+        private int tempo;
+
+        [Column]
+        public int Tempo
+        {
+            get
+            {
+                return tempo;
+            }
+            set
+            {
+                if (value != tempo)
+                {
+                    tempo = value;
+                    NotifyPropertyChanged("Tempo");
+                }
+            }
+        }
+        #endregion
+
+
+        #region NotifyProperty
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
--- AdamsCronometro/MainPage.xaml.cs
+++ AdamsCronometro/MainPage.xaml.cs
@@ -8,34 +8,65 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using AdamsCronometro.Resources;
+using System.Windows.Threading;
 
 namespace AdamsCronometro
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private DispatcherTimer timer = new DispatcherTimer();
+        private int tempo = 0;
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
 
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
+            /* Construcao do Timer */
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += timer_Tick;
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            /* Tempo @todo mudar */
+            tempo += 1;
+            atualizaLabel();
 
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
+            /* Atualiza o ponteiro do cronometro */
+            PointerAngle.Angle += 6;
+        }
 
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+        private void atualizaLabel() {
+            int minutos = tempo / 60;
+            int segundos = tempo % 60;
+
+            lblTempo.Text = minutos.ToString("00") + ":" + segundos.ToString("00");
+        }
+
+        // Metodo para iniciar o cronometro e registrar os tempos 
+        private void Cronometro_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            if (timer.IsEnabled)
+            {
+                timer.Stop();
+            }
+            else 
+            {
+                timer.Start();
+            }
+        }
+
+        // Metodo para para o cronometro.
+        private void Cronometro_DoubleTap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            timer.Stop();
+
+            tempo = 0;
+            atualizaLabel();
+            
+            PointerAngle.Angle = 0;
+            // Pivot.SelectedIndex = 1;
+        }
     }
 }
